7cbecb8669b81291698319f123a70841a9c7678b
Attia33 <attia3003@gmail.com>
2024-08-27 04:29:54 +0300

    devos

---
+++ 0x16-api_advanced/2-recurse.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+import requests
+
+
+def recurse(subreddit, hot_list=[], after=""):
+    """Recursively fetches all hot articles for a subreddit."""
+    url = f"https://www.reddit.com/r/{subreddit}/hot.json"
+    headers = {'User-Agent': 'my-custom-user-agent'}
+    params = {'after': after}
+
+
+    try:
+        response = requests.get(url, headers=headers, params=params, allow_redirects=False)
+        if response.status_code == 200:
+            data = response.json()
+            posts = data['data']['children']
+            after = data['data']['after']
+            if not posts:
+                return hot_list
+            hot_list.extend([post['data']['title'] for post in posts])
+            if after:
+                return recurse(subreddit, hot_list, after)
+            return hot_list
+        else:
+            return None
+         except requests.RequestException:
+             return None
